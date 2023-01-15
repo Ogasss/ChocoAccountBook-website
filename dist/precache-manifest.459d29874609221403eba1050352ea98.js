f3f3ad6f2e971ba210bc6d784b5abf94022325c1
Ogas <83815549+Unclotho@users.noreply.github.com>
2023-01-16 00:55:39 +0800

    构建

---
+++ dist/precache-manifest.459d29874609221403eba1050352ea98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2299b4105a9ae365d559",
+    "url": "/ChocoAccountBook-website/css/app.5c930da1.css"
+  },
+  {
+    "revision": "1e20dd90595503aed46ead2a1fca0b23",
+    "url": "/ChocoAccountBook-website/index.html"
+  },
+  {
+    "revision": "2299b4105a9ae365d559",
+    "url": "/ChocoAccountBook-website/js/app.cec327b3.js"
+  },
+  {
+    "revision": "80dccdaa180665600c16",
+    "url": "/ChocoAccountBook-website/js/chunk-vendors.8f7f3774.js"
+  },
+  {
+    "revision": "544062a307aed2ea9cd1a6e74af38e96",
+    "url": "/ChocoAccountBook-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ChocoAccountBook-website/robots.txt"
+  }
+]);
